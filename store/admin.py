f7f7234bc9f02ae7b2e79e150a51e75027581ad5
Biola <52200708+decorouz@users.noreply.github.com>
2022-03-07 10:50:32 +0100

    setup admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Author, Category, Product
 
 # Register your models here.
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    prepopulated_fields = {"slug": ("last_name", "first_name")}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "slug", "price", "in_stock", "created", "updated"]
+
+    list_filter = ["in_stock", "is_active"]
+    list_editable = ["in_stock", "price"]
+    prepopulated_fields = {"slug": ("title", "author")}
--- store/models.py
+++ store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name}, {self.last_name}"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="product", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, db_index=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    slug = models.SlugField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
