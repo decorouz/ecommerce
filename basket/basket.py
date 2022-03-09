3034396e2b7f671ec2563d2ac7046fb5460c3777
Biola <52200708+decorouz@users.noreply.github.com>
2022-03-09 23:47:06 +0100

    ajax crud

--- basket/basket.py
+++ basket/basket.py
@@ -1,7 +1,11 @@
+from decimal import Decimal
+from itertools import product
+from store.models import Product
+
+
 class Basket(object):
     """
-    A base Basket class, providing some default behaviors that can be inherited or
-    overrided, as necessary
+    A base Basket class, providing some default behaviors that can be inherited or overrided, as necessary
     """
 
     def __init__(self, request) -> None:
@@ -15,15 +19,58 @@ class Basket(object):
         """
         Add product to cart and update it quantity
         """
-        product_id = product.id
+        product_id = str(product.id)
 
-        if product_id not in self.basket:
-            self.basket[product_id] = {"price": str(product.price), "qty": int(qty)}
+        if product_id in self.basket:
+            self.basket[product_id]["qty"] = qty
+        else:
+            self.basket[product_id] = {"price": str(product.price), "qty": qty}
+
+        self.save()
+
+    def save(self):
         self.session.modified = True
-        print(self.basket)
 
     def __len__(self):
         """
         Get the basket data and get the quantity of items
         """
         return sum([item["qty"] for item in self.basket.values()])
+
+    def __iter__(self):
+        """Collect the product id in the session data to query
+        the database and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]["product"] = product
+
+        for item in basket.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
+            yield item
+
+    def get_total_price(self):
+        return sum([Decimal(item["price"]) * item["qty"] for item in self.basket.values()])
+
+    def delete(self, product):
+        """
+        Delete a selected product from session data
+        """
+        product_id = str(product)
+        print(product_id)
+        if product_id in self.basket:
+            del self.basket[product_id]
+        self.save()
+
+    def update(self, product, qty):
+        """
+        update values in session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]["qty"] = qty
+        self.save()
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+import re
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from store.models import Product
 
@@ -17,5 +18,31 @@ def basket_add(request):
         product_qty = int(request.POST.get("productqty"))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
-        response = JsonResponse({"qty": product_qty})
+        basket_qty = basket.__len__()
+        response = JsonResponse({"qty": basket_qty})
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({"qty": basketqty, "subtotal": baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({"qty": basketqty, "subtotal": baskettotal})
         return response
