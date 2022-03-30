fc7869bffa1043f630b9c6b1e28bfa8fb3161a12
Biola <52200708+decorouz@users.noreply.github.com>
2022-03-30 14:15:44 +0100

    nc

--- account/tests/test_store_model.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from django.urls import reverse
-
-
-def test_category_str(product_category):
-    assert product_category.__str__() == "django"
--- basket/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField
-    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
