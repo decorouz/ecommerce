10e56caac85d526c53e2908c640fe5eef5945bc5
Biola <52200708+decorouz@users.noreply.github.com>
2022-03-31 10:12:45 +0100

    phase 1

---
+++ ecommerce/tests/selenium.py
@@ -0,0 +1,14 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def chrome_browser_instance(request):
+    "Provide selenium web browser instance"
+    options = Options()
+    options.headless = False
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.close()
